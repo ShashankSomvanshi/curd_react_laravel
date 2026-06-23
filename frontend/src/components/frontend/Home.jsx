13b493614bfdc9a06fe8059d68422d0f323e396e
Shashank Somvanshi <somvanshishashank29@gmail.com>
2026-06-23 23:23:59 +0530

    Completed Project and Service Section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -4,12 +4,14 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import AboutImg from "../../assets/images/about-us.jpg";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
-
+import ServiceImg from "../../assets/images/construction1.jpg";
+import ConstructionImg from "../../assets/images/construction2.jpg";
+import Icon1 from "../../assets/images/icon-1.svg";
 
 function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <main>
         {/* Hero Section */}
         <section className="section-1">
@@ -61,9 +63,289 @@ function Home() {
             </div>
           </div>
         </section>
+        {/* Our Services */}
+        <section className="section-3 bg-light py-5">
+          <div className="container-fluid py-5">
+            <div className="section-header text-center">
+              <span>our services</span>
+              <h2>Our construction services</h2>
+              <p>
+                We offer a diverse array of construction services, spanning
+                residential, commercial, and industrial projects.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Specialty Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Specialty Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Specialty Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ServiceImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Specialty Construction</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Why Choose Us */}
+        <section className="section-4 py-5">
+          <div className="container py-5">
+            <div className="section-header text-center">
+              <span>Why Choose Us</span>
+              <h2>Discover our wide variety of projects.</h2>
+              <p>
+                Created in close partnership with our clients and collaborators,
+                this approach merges industry expertise, <br />
+                decades of experience, innovation, and flexibility to
+                consistently deliver excellence.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon1} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting Edge Solution</h3>
+                  </div>
+
+                  <p>
+                    Small actions create big impacts. It all begins and ends
+                    with each employee committing to safer work practices daily,
+                    ensuring they return home safely.
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon1} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting Edge Solution</h3>
+                  </div>
+
+                  <p>
+                    Small actions create big impacts. It all begins and ends
+                    with each employee committing to safer work practices daily,
+                    ensuring they return home safely.
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="card shadow border-0 p-4">
+                  <div className="card-icon">
+                    <img src={Icon1} alt="" />
+                  </div>
+                  <div className="card-title mt-3">
+                    <h3>Cutting Edge Solution</h3>
+                  </div>
+
+                  <p>
+                    Small actions create big impacts. It all begins and ends
+                    with each employee committing to safer work practices daily,
+                    ensuring they return home safely.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* Our Projects */}
+        <section className="section-3 bg-light py-5">
+          <div className="container-fluid py-5">
+            <div className="section-header text-center">
+              <span>our projects</span>
+              <h2>Discover our diverse range of projects</h2>
+              <p>
+                We offer a diverse array of construction services, spanning
+                residential, commercial, and industrial projects.
+              </p>
+            </div>
+            <div className="row pt-4">
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Kolkata Projects</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Kolkata Projects</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Kolkata Projects</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3 col-lg-3">
+                <div className="item">
+                  <div className="service-image">
+                    <img src={ConstructionImg} alt="" className="w-100" />
+                  </div>
+                  <div className="service-body">
+                    <div className="service-title">
+                      <h3>Kolkata Projects</h3>
+                    </div>
+                    <div className="service-content">
+                      <p>
+                        Building construction is a broad and essential sector
+                        within the construction industry that focuses on the
+                        creation of structures designed for human occupancy and
+                        use.
+                      </p>
+                    </div>
+                    <a href="" className="btn btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
       {/* Footer Section */}
-      <Footer/>
+      <Footer />
     </>
   );
 }
