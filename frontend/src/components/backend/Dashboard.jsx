f8bf30945747698cfbef802d32bdc908b9b48d74
shashank somvanshi <somvanshishashank29@gmail.com>
2026-06-30 17:42:45 +0530

    work on context hooks

--- frontend/src/components/backend/Dashboard.jsx
+++ frontend/src/components/backend/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import Sidebar from "../common/Sidebar";
 
 const Dashboard = () => {
   return (
@@ -10,13 +11,8 @@ const Dashboard = () => {
         <div className="container my-5">
           <div className="row">
             <div className="col-md-3">
-                {/* sidebar */}
-                <div className="card shadow border-0">
-                    <div className="card-body">
-                    <h4>Sidebar</h4>
-                    </div>
-              </div>
-
+              {/* sidebar */}
+              <Sidebar/>
             </div>
             <div className="col-md-9 dashboard">
               {/* Dashboard */}
